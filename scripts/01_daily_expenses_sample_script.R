662568704f696b65804cda139cc196eb336d4287
Volans01 <jayrmaguad.01@gmail.com>
2026-05-17 12:24:22 +0800

    Update 01_daily_expenses_sample_script.R

--- scripts/01_daily_expenses_sample_script.R
+++ scripts/01_daily_expenses_sample_script.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-data <- read.csv("/storage/emulated/0/R/daily_expenses_sample.csv", stringsAsFactors = FALSE)
+data <- read.csv("01_daily_expenses_sample_data.csv", stringsAsFactors = FALSE)
 
 # clean names
 colnames(data) <- c("Date", "Type", "Product", "Price")
@@ -13,4 +13,4 @@ sum_dat <- aggregate(Price ~ Type, data = data, sum)
 ggplot(sum_dat, aes(x = Type, y = Price)) +
   geom_bar(stat = "identity")
 
-ggsave("plot.png")
+ggsave("01_daily_expenses_plot.png")
